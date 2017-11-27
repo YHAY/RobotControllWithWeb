f704b6a65fa4f53b8c70b21c89cac34a543e33a0
YHAY <dbsgodl@naver.com>
2017-11-27 09:58:24 +0900

    Add files via upload

--- rcww/script/move_controller.py
+++ rcww/script/move_controller.py
@@ -34,20 +34,13 @@ class Move_controller():
         self.__have_odom = False
         
         self.tf_listener = tf.TransformListener()
-        
-        #라이더가 장애물을 발견하고 발견한 횟수가 5번을 넘어가면 확실히 장애물을 인식한 것으로 취급
-        self.__ridar_cnt = 0
-
-        self.on_bumper = None
         self.movement_enabled = True
-        
-        #주차중에 장애물 여부를 확인하기 위한 변수
-        self.chk_obstacle = False
 
         self.__odom_sub = rospy.Subscriber('/odom', Odometry, self.__odom_handler)
-        self.__scan_sub = rospy.Subscriber('/scan', LaserScan, self.__scan_handler)
+        #self.__scan_sub = rospy.Subscriber('/scan', LaserScan, self.__scan_handler)
         self.__cmd_vel_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size = 1)
 
+        #명령에 맞게 움직이기 위해 필요한 변수들
         self.forward_state = False
         self.backward_state = False
         self.right_state = False
@@ -67,15 +60,17 @@ class Move_controller():
         self.left_pub = rospy.Publisher('/tb/control/left', Bool, queue_size = 1)
         self.stop_pub = rospy.Publisher('/tb/control/stop', Bool, queue_size = 1)
 
+    #전진 명령을 받았을 때
     def forward_callback(self,msg):
         self.forward_state = msg.data
         print "self.forward_state", self.forward_state
-        if self.forward_state and (not self.stop_state):
+        if self.forward_state:
             self.go_forward()
+            #앞으로 가는 명령을 실행시킨 뒤 다시 상태를 바꿔줘야 다음에 또 사용가능해진다.
             self.forward_state = False
             self.forward_pub.publish(self.forward_state)
 
-        
+    #후진 명령을 받았을 때
     def backward_callback(self,msg):
         self.backward_state = msg.data
         print "self.backward_state", self.backward_state
@@ -84,7 +79,7 @@ class Move_controller():
             self.backward_state = False
             self.backward_pub.publish(self.backward_state)
 
-
+    #우회전 명령을 받았을 때
     def right_callback(self,msg):
         self.right_state = msg.data
         print "self.right_state", self.right_state
@@ -93,6 +88,7 @@ class Move_controller():
             self.right_state = False
             self.right_pub.publish(self.right_state)
 
+    #좌회전 명령을 받았을 때
     def left_callback(self,msg):
         self.left_state = msg.data
         print "self.left_state", self.left_state
@@ -101,6 +97,7 @@ class Move_controller():
             self.left_state = False
             self.left_pub.publish(self.left_state)
 
+    #정지 명령을 받았을 때
     def stop_callback(self,msg):
         self.stop_state = msg.data
         print "self.stop_state", self.stop_state
@@ -116,86 +113,41 @@ class Move_controller():
           self.stop_state = False
           self.stop_pub.publish(self.stop_state)
 
-
-    def move(self, linear=0.0, angular=0.0):
-        """Moves the robot at a given linear speed and angular velocity
-
-        The speed is in meters per second and the angular velocity is in radians per second
-
-        """
-        self.__exit_if_movement_disabled()
-        # Bounds checking
-        if abs(linear) > self.max_linear:
-            linear = self.max_linear if linear > self.max_linear else linear
-            linear = -self.max_linear if linear < -self.max_linear else linear
-        if abs(angular) > self.max_angular:
-            angular = self.max_angular if angular > self.max_angular else angular
-            angular = -self.max_angular if angular < -self.max_angular else angular
-        # Message generation
-        msg = Twist()
-        msg.linear.x = linear
-        msg.angular.z = angular
-        # Announce and publish
-        self.__cmd_vel_pub.publish(msg)
-
+    #직선위를 움직이는 걸 조절하는 부분
     def move_distance(self, distance, velocity=0.05):
-        """Moves a given distance in meters
-
-        You can also give it a speed in meters per second to travel at:
-
-            robot.move_distance(1, 0.5)  # Should take 2 seconds
-        """
         
-#        print "move_distance entered"
         self.__exit_if_movement_disabled()
-        print "move_distance entered, distance : " + str(distance)
-        # No bounds checking because we trust people. Not like William.
         r = rospy.Rate(1)
-        print "move_distance entered2"
         while not self.__have_odom and not rospy.is_shutdown():
             r.sleep()
-        print "move_distance entered3"
+
         msg = Twist()
         if distance < 0 :
           msg.linear.x = velocity * (-1)
         else : 
           msg.linear.x = velocity
-        print "move_distance entered4"
+
         x0 = self.__x
         y0 = self.__y
         r = rospy.Rate(30)
-        print "move_distance entered5"
+
         while not rospy.is_shutdown():
             d = ((self.__x - x0)**2 + (self.__y - y0)**2)**0.5
-            print "move_distance entered6"
             if self.stop_state : 
                 break
     
             if d >= abs(distance):
-                print "move_distance entered7"
                 break
 
             self.__cmd_vel_pub.publish(msg)
-            print "move_distance entered8"
             r.sleep()
         msg.linear.x = 0.0
-        print "move_distance entered9"
         self.__cmd_vel_pub.publish(msg)
 
-
+    #회전을 조절하는 부분
     def turn_angle(self, angle, velocity=0.22):
-        """Turns the robot a given number of degrees in radians
-
-        You can easily convert degress into radians with the radians() function:
-
-            robot.turn_angle(radians(45))  # Turn 45 degrees
-
-        You can also give an angular velocity to turn at, in radians per second:
-
-            robot.turn_angle(radians(-45), radians(45))  # Turn back over a second
-        """
+	# 단위는 radian으로 좀 더 섬세하게 작동시키고 싶으면 바로 숫자를 적기보다 radian(45)와 같은 함수를 쓰자!
         self.__exit_if_movement_disabled()
-        # No bounds checking because we trust people. Not like William.
         r = rospy.Rate(1)
         while not self.__have_odom and not rospy.is_shutdown():
             r.sleep()
@@ -206,10 +158,13 @@ class Move_controller():
         else:
             msg.angular.z = -np.abs(velocity)
         angle0 = self.__cumulative_angle
+
         r = rospy.Rate(100)
         while not rospy.is_shutdown():
+            #정지상태가 발생하면 무조건 그만두기
             if self.stop_state : 
               break
+            #아직 움직여야하는 회전정도를 구하기 위해서 (돌아야 할 각 - 남은 각)을 계산한다.
             a_diff = self.__cumulative_angle - angle0
             if (angle > 0 and a_diff >= angle) or (angle < 0 and a_diff <= angle):
               break
@@ -219,9 +174,8 @@ class Move_controller():
         msg.angular.z = 0.0
         self.__cmd_vel_pub.publish(msg)
 
-
+    #로봇 작동에 잠깐의 멈춤을 주고 다시 움직이고 싶을 때, 정지와 달리 멈췄다가 다시 동작을 이어서 하는 것이 가능하다.
     def wait(self, seconds):
-        """This function will wait for a given number of seconds before returning"""
         time.sleep(seconds)
 
     
@@ -254,18 +208,6 @@ class Move_controller():
           if (245-range_len) <= i <= (315-range_len) :
               return True
 
-    #라이다 장애물 확인
-    def __scan_handler(self, msg):
-         
-          if self.is_ac(msg) :
-            self.chk_obstacle = True
-            self.__ridar_cnt = self.__ridar_cnt + 1
-          else:
-            self.chk_obstacle = False
-            
-          if self.__ridar_cnt > 5:
-            self.chk_obstacle = True
-          #print self.chk_obstacle
           
     #움직임이 제어가 되지 않을 때 발생
     def __exit_if_movement_disabled(self):
@@ -291,14 +233,14 @@ class Move_controller():
         self.move_distance(-0.1)#backward
         self.wait(0.6)
 
-    #로봇이 옆으로(우) 돌기위해 명령전달, 약 90도 회전
+    #로봇이 옆으로(우) 돌기위해 명령전달, 약 45도 회전
     def turn_right(self):
-        self.turn_angle(1.58)
+        self.turn_angle(0.79)
         self.wait(0.6)
 
-    #로봇이 옆으로(좌) 돌기위해 명령전달, 약 90도 회전
+    #로봇이 옆으로(좌) 돌기위해 명령전달, 약 45도 회전
     def turn_left(self):
-        self.turn_angle(-1.58)
+        self.turn_angle(-0.79)
         self.wait(0.6)
 
 if __name__ == '__main__':
